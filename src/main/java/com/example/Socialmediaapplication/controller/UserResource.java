91523740c89229738d0348677a2e027dc4ce6ba8
Shivakant6387 <shivakant5875@gmail.com>
2023-05-16 17:31:56 +0530

    get method using path variable

--- src/main/java/com/example/Socialmediaapplication/controller/UserResource.java
+++ src/main/java/com/example/Socialmediaapplication/controller/UserResource.java
@@ -4,6 +4,7 @@ import com.example.Socialmediaapplication.DaoService.UserDaoService;
 import com.example.Socialmediaapplication.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +19,9 @@ public class UserResource {
     @GetMapping("/users")
     public List<User>retrieveAllUsers(){
         return service.findAll();
+    }
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id){
+        return service.findOne(id);
     }
 }
