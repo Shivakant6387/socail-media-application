716882b8e80d8444c2125671d77d6c28b3b18390
Shivakant6387 <shivakant5875@gmail.com>
2023-05-17 17:15:25 +0530

    uri response entity

--- src/main/java/com/example/Socialmediaapplication/DaoService/UserDaoService.java
+++ src/main/java/com/example/Socialmediaapplication/DaoService/UserDaoService.java
@@ -11,13 +11,13 @@ import java.util.function.Predicate;
 @Component
 public class UserDaoService {
     private static List<User>users=new ArrayList<>();
-
+    private static int userCount=0;
     static {
-        users.add(new User(1,"adam", LocalDate.now().minusYears(30)));
-        users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(3,"Jim", LocalDate.now().minusYears(20)));
-        users.add(new User(4,"mac", LocalDate.now().minusYears(15)));
-        users.add(new User(5,"maki", LocalDate.now().minusYears(10)));
+        users.add(new User(++userCount,"adam", LocalDate.now().minusYears(30)));
+        users.add(new User(++userCount,"Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(++userCount,"Jim", LocalDate.now().minusYears(20)));
+        users.add(new User(++userCount,"mac", LocalDate.now().minusYears(15)));
+        users.add(new User(++userCount,"maki", LocalDate.now().minusYears(10)));
     }
     public List<User>findAll(){
         return users;
@@ -25,6 +25,11 @@ public class UserDaoService {
     public User findOne(int id){
         Predicate<? super User> predicate=user -> user.getId().equals(id);
         User user = users.stream().filter(predicate).findFirst().get();
+        return user;
+    }
+    public User save(User user){
+        user.setId(++userCount);
+        users.add(user);
         return user;
     }
 }
--- src/main/java/com/example/Socialmediaapplication/controller/UserResource.java
+++ src/main/java/com/example/Socialmediaapplication/controller/UserResource.java
@@ -3,10 +3,11 @@ package com.example.Socialmediaapplication.controller;
 import com.example.Socialmediaapplication.DaoService.UserDaoService;
 import com.example.Socialmediaapplication.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,5 +24,11 @@ public class UserResource {
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id){
         return service.findOne(id);
+    }
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User save = service.save(user);
+        URI location= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(save.getId()).toUri();
+        return ResponseEntity.created(location).build();
     }
 }
